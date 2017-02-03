0683b16d7f38bab102ec816988daa178b8cc5984
Manohar <manohar.bhavandla@outlook.com>
2017-02-03 21:37:41 +0000

    Added new views to hello.py

--- trunk/hello.py
+++ trunk/hello.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World! welcome'
+
+@app.route('/index')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
     
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=8080)
